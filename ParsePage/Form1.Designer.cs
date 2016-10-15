f64fc566bafd2e557e6c57520a0c75a3770615fb
alex-67 <ankurilenkov_1@edu.hse.ru>
2016-10-15 12:26:21 +0300

    Добавьте файлы проекта.

---
+++ ParsePage/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace ParsePage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_download = new System.Windows.Forms.Button();
+            this.textBox_URL = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.скопироватьHTMLКодToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьКакToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button_clear = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button_stop = new System.Windows.Forms.Button();
+            this.button_openFolder = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_pathToImages = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button_download
+            // 
+            this.button_download.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button_download.Location = new System.Drawing.Point(0, 451);
+            this.button_download.Name = "button_download";
+            this.button_download.Size = new System.Drawing.Size(906, 23);
+            this.button_download.TabIndex = 0;
+            this.button_download.Text = "Скачать";
+            this.button_download.UseVisualStyleBackColor = true;
+            this.button_download.Click += new System.EventHandler(this.button_download_Click);
+            // 
+            // textBox_URL
+            // 
+            this.textBox_URL.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_URL.Location = new System.Drawing.Point(87, 28);
+            this.textBox_URL.Name = "textBox_URL";
+            this.textBox_URL.Size = new System.Drawing.Size(698, 20);
+            this.textBox_URL.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 140);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(906, 305);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "URL к товарам";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(906, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.скопироватьHTMLКодToolStripMenuItem,
+            this.сохранитьКакToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // скопироватьHTMLКодToolStripMenuItem
+            // 
+            this.скопироватьHTMLКодToolStripMenuItem.Name = "скопироватьHTMLКодToolStripMenuItem";
+            this.скопироватьHTMLКодToolStripMenuItem.Size = new System.Drawing.Size(204, 22);
+            this.скопироватьHTMLКодToolStripMenuItem.Text = "Скопировать HTML код";
+            this.скопироватьHTMLКодToolStripMenuItem.Click += new System.EventHandler(this.скопироватьHTMLКодToolStripMenuItem_Click);
+            // 
+            // сохранитьКакToolStripMenuItem
+            // 
+            this.сохранитьКакToolStripMenuItem.Name = "сохранитьКакToolStripMenuItem";
+            this.сохранитьКакToolStripMenuItem.Size = new System.Drawing.Size(204, 22);
+            this.сохранитьКакToolStripMenuItem.Text = "Сохранить как";
+            this.сохранитьКакToolStripMenuItem.Click += new System.EventHandler(this.сохранитьКакToolStripMenuItem_Click);
+            // 
+            // button_clear
+            // 
+            this.button_clear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_clear.Location = new System.Drawing.Point(791, 26);
+            this.button_clear.Name = "button_clear";
+            this.button_clear.Size = new System.Drawing.Size(112, 23);
+            this.button_clear.TabIndex = 6;
+            this.button_clear.Text = "Очистить таблицу";
+            this.button_clear.UseVisualStyleBackColor = true;
+            this.button_clear.Click += new System.EventHandler(this.button_clear_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(4, 97);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(782, 23);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // button_stop
+            // 
+            this.button_stop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_stop.Location = new System.Drawing.Point(792, 97);
+            this.button_stop.Name = "button_stop";
+            this.button_stop.Size = new System.Drawing.Size(112, 23);
+            this.button_stop.TabIndex = 8;
+            this.button_stop.Text = "Остановить";
+            this.button_stop.UseVisualStyleBackColor = true;
+            this.button_stop.Click += new System.EventHandler(this.button_stop_Click);
+            // 
+            // button_openFolder
+            // 
+            this.button_openFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_openFolder.Location = new System.Drawing.Point(791, 55);
+            this.button_openFolder.Name = "button_openFolder";
+            this.button_openFolder.Size = new System.Drawing.Size(112, 23);
+            this.button_openFolder.TabIndex = 11;
+            this.button_openFolder.Text = "Открыть";
+            this.button_openFolder.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Путь к папке";
+            // 
+            // textBox_pathToImages
+            // 
+            this.textBox_pathToImages.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_pathToImages.Location = new System.Drawing.Point(87, 57);
+            this.textBox_pathToImages.Name = "textBox_pathToImages";
+            this.textBox_pathToImages.Size = new System.Drawing.Size(698, 20);
+            this.textBox_pathToImages.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(906, 474);
+            this.Controls.Add(this.button_openFolder);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_pathToImages);
+            this.Controls.Add(this.button_stop);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button_clear);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox_URL);
+            this.Controls.Add(this.button_download);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Parse Lamoda";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_download;
+        private System.Windows.Forms.TextBox textBox_URL;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem скопироватьHTMLКодToolStripMenuItem;
+        private System.Windows.Forms.Button button_clear;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьКакToolStripMenuItem;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button_stop;
+        private System.Windows.Forms.Button button_openFolder;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_pathToImages;
+    }
+}
+
---
+++ ParsePage/Form1.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+namespace ParsePage
+{
+    public partial class Form1 : Form
+    {
+        private string url;
+        WebBrowser wb;
+        public Form1()
+        {
+            InitializeComponent();
+            //textBox_URL.Text = @"http://www.lamoda.ru/cb/479-23837/clothes-muzhskaya-verkhnyaya-odezhda-topman/?order=ORDER_BY_NEW";
+            textBox_URL.Text = @" http://www.lamoda.ru/c/157/shoes-muzhskie-tufli/?genders=men&sitelink=topmenuM&l=4";
+            textBox_pathToImages.Text = @"C:\All images\";
+            makeTable();
+            dataGridView1.DataSource = table;
+            dataGridView1.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            dataGridView1.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridView1.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridView1.Columns[3].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            dataGridView1.Columns[4].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            //&page=2
+            //<span class="products-catalog__head-counter">59 товаров</span>
+            url = textBox_URL.Text;
+            isStoped = false;
+        }
+
+        private string Win1251ToUTF8(string source)
+        {
+            Encoding utf8 = Encoding.GetEncoding("utf-8");
+            Encoding win1251 = Encoding.GetEncoding("windows-1251");
+            byte[] utf8Bytes = win1251.GetBytes(source);
+            byte[] win1251Bytes = Encoding.Convert(win1251, utf8, utf8Bytes);
+            source = win1251.GetString(win1251Bytes);
+            return source;
+        }
+        private void button_download_Click(object sender, EventArgs e)
+        {
+
+            url = textBox_URL.Text;
+            currPage = 1;
+            pagesCount = 0;
+            countImages = 0;
+            progressBar1.Value = 0;
+            wb = new WebBrowser();
+            wb.AllowNavigation = true;
+            wb.ScriptErrorsSuppressed = true;
+            wb.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(wb_DocumentCompleted);
+            wb.Navigate(url);
+        }
+        
+        int pagesCount;
+        int currPage=1;
+        private void wb_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            WebBrowser wb = sender as WebBrowser;
+            if (wb.ReadyState != WebBrowserReadyState.Complete)
+                return;
+            if (e.Url.AbsolutePath != (sender as WebBrowser).Url.AbsolutePath)
+                return;
+            HtmlDocument doc = wb.Document;
+            //textBox_inner.Text = doc.Body.InnerText;
+
+
+            // wb.Document is not null at this point
+            if (pagesCount==0)
+                foreach (HtmlElement elm in ElementsByClass(doc, "products-catalog__head-counter"))
+                {//<span class="products-catalog__head-counter">59 товаров</span>
+                 //19941 товар
+                
+                    string str = elm.InnerText;
+                    str=str.Replace(" товаров", "").Replace(" товара", "").Replace(" товар", "");
+                    pagesCount = (Int32.Parse(str) / 60 + ((Int32.Parse(str) % 60) != 0 ? 1 : 0)) ;
+                    progressBar1.Minimum = 0;
+                    progressBar1.Maximum = pagesCount;
+                    break;
+                }
+            if (pagesCount>1 && currPage < pagesCount)
+            {
+                //&page=2
+
+                if (!isStoped)
+                {
+                    currPage++;
+                    wb.Navigate(url + "&page=" + currPage);
+                }
+             }
+            parseProducts(doc);
+            progressBar1.Increment(1);
+
+        }
+        private static string MakeValidFileName(string name)
+        {
+            string invalidChars = System.Text.RegularExpressions.Regex.Escape(new string(System.IO.Path.GetInvalidFileNameChars()));
+            string invalidRegStr = string.Format(@"([{0}]*\.+$)|([{0}]+)", invalidChars);
+
+            return System.Text.RegularExpressions.Regex.Replace(name, invalidRegStr, "_");
+        }
+        int countImages;
+        void parseProducts(HtmlDocument doc)
+        {
+            HtmlElementCollection elms = doc.GetElementsByTagName("div");
+            DataRow row;
+            List<string> srcs = new List<string>();
+            //products-list-item__img
+            /*
+            <img class="products-list-item__img" width="236" height="341"
+            src="//pi0.lmcdn.ru/img236x341/B/U/BU182AMJKR77_1_v1.jpg"
+            alt="Туфли Bugatti, цвет: коричневый. Артикул: BU182AMJKR77. Мужская обувь / Туфли" 
+            src-rollover="//pi2.lmcdn.ru/img236x341/B/U/BU182AMJKR77_2_v1.jpg" 
+            data-src="//pi0.lmcdn.ru/img236x341/B/U/BU182AMJKR77_1_v1.jpg">
+            */
+            string localFolder = textBox_pathToImages.Text;
+            if(!System.IO.Directory.Exists(localFolder))
+                System.IO.Directory.CreateDirectory(localFolder);
+            using (WebClient client = new WebClient())
+            {
+                int count_img = doc.GetElementsByTagName("img").Count;
+                foreach (HtmlElement elm in ElementsByClass(doc, "products-list-item__img lazy"))
+                {
+                    //"//pi0.lmcdn.ru/img236x341/V/A/VA468AMICV22_1_v1.jpg"
+                    countImages += 1;
+                    string src = elm.GetAttribute("data-src");
+                    src = src.Replace("//", "http://");
+                    string name = localFolder + MakeValidFileName(src);
+                    if (System.IO.File.Exists(name))
+                        name = newFileName(name,countImages);
+                    client.DownloadFile(src, name);
+                }
+                foreach (HtmlElement elm in ElementsByClass(doc, "products-list-item__img"))
+                {
+                    countImages += 1;
+                    string src = elm.GetAttribute("src");
+                    string name = localFolder + MakeValidFileName(src);
+                    if (System.IO.File.Exists(name))
+                        name = newFileName(name, countImages);
+                    client.DownloadFile(src, name);
+                }
+            }
+
+            foreach (HtmlElement elm in ElementsByClass(doc, "products-list-item__brand"))
+            {
+                row = table.NewRow();
+                row["id"] = curr_id;
+                row["Brand"] = elm.InnerText;
+                //
+                if (elm.NextSibling != null)
+                {
+
+
+                    row["ProductName"] = elm.NextSibling.InnerText;
+                    /*
+                    <span class="price">
+                        <span class="price__old">7 900 руб.</span><wbr></wbr><span class="price__new" itemprop="price">7 110 руб.</span>
+                        <link itemprop="availability" href="http://schema.org/InStock">
+                    </span>
+
+                    <span class="button button_s button_outline paginator__prev">Назад</span><span class="paginator__pages"></span>
+                    <span class="button button_s button_outline paginator__next">Дальше</span>
+
+
+                    ///itemprop="price"
+        */
+                    if (!(elm.NextSibling.NextSibling.GetAttribute("itemprop") == "price"))
+                    {
+                        foreach (HtmlElement pr in elm.NextSibling.NextSibling.Children)
+                        {
+                            if (pr.GetAttribute("className") == "price__old") row["OldPrice"] = ParseInt(pr.InnerText);
+                            if (pr.GetAttribute("className") == "price__new") row["NewPrice"] = ParseInt(pr.InnerText);
+                        }
+
+                    }
+                    else
+                    {
+                        row["OldPrice"] = ParseInt(elm.NextSibling.NextSibling.InnerText);
+                        row["NewPrice"] = ParseInt(elm.NextSibling.NextSibling.InnerText);
+                    }
+                }
+                table.Rows.Add(row);
+                curr_id++;
+            }
+        }
+
+        private static string newFileName(string name, int count_)
+        {
+            string fileNameOnly = System.IO.Path.GetFileNameWithoutExtension(name);
+            string extension = System.IO.Path.GetExtension(name);
+            string path = System.IO.Path.GetDirectoryName(name);
+            string tempFileName = string.Format("{0}({1})", fileNameOnly, count_);
+            return System.IO.Path.Combine(path, tempFileName + extension);
+        }
+
+        int ParseInt(string str)
+        {
+            str = str.Replace(" ","");
+            return Int32.Parse(str.Replace("руб.", ""));
+        }
+        static IEnumerable<HtmlElement> ElementsByClass(HtmlDocument doc, string className)
+        {
+            foreach (HtmlElement e in doc.All)
+                if (e.GetAttribute("className") == className)
+                    yield return e;
+        }
+        System.Data.DataTable table;
+        int curr_id;
+        private void makeTable()
+        {
+            // Create a new DataTable.
+            table = new DataTable("ProductsTable");
+            // Declare variables for DataColumn and DataRow objects.
+            DataColumn column;
+
+            // Create new DataColumn, set DataType, 
+            // ColumnName and add to DataTable.    
+            column = new DataColumn();
+            column.DataType = System.Type.GetType("System.Int32");
+            column.ColumnName = "id";
+            //column.ReadOnly = true;
+            column.Unique = true;
+            // Add the Column to the DataColumnCollection.
+            table.Columns.Add(column);
+
+            // Create second column.
+            column = new DataColumn();
+            column.DataType = System.Type.GetType("System.String");
+            column.ColumnName = "Brand";
+            column.AutoIncrement = false;
+            column.Caption = "Brand";
+            column.ReadOnly = false;
+            column.Unique = false;
+            table.Columns.Add(column);
+
+            column = new DataColumn();
+            column.DataType = System.Type.GetType("System.String");
+            column.ColumnName = "ProductName";
+            column.AutoIncrement = false;
+            column.Caption = "Product name(type)";
+            column.ReadOnly = false;
+            column.Unique = false;
+            table.Columns.Add(column);
+
+            column = new DataColumn();
+            column.DataType = System.Type.GetType("System.Int32");
+            column.ColumnName = "OldPrice";
+            column.AutoIncrement = false;
+            column.Caption = "Old price";
+            column.ReadOnly = false;
+            column.Unique = false;
+            column.DefaultValue = 0;
+            table.Columns.Add(column);
+
+            column = new DataColumn();
+            column.DataType = System.Type.GetType("System.Int32");
+            column.ColumnName = "NewPrice";
+            column.AutoIncrement = false;
+            column.Caption = "New price";
+            column.ReadOnly = false;
+            column.Unique = false;
+            column.DefaultValue = 0;
+            // Add the column to the table.
+            table.Columns.Add(column);
+
+            // Make the ID column the primary key column.
+            DataColumn[] PrimaryKeyColumns = new DataColumn[1];
+            PrimaryKeyColumns[0] = table.Columns["id"];
+            table.PrimaryKey = PrimaryKeyColumns;
+
+        }
+
+        private void скопироватьHTMLКодToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //if(doc!=null)
+            // Clipboard.SetText(doc.Body.OuterText);
+        }
+
+        private void button_clear_Click(object sender, EventArgs e)
+        {
+            table.Clear();
+            curr_id = 0;
+
+        }
+
+        private void сохранитьКакToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "XML-File | *.xml";
+            if (sfd.ShowDialog()==DialogResult.OK)
+            {
+                table.WriteXml(sfd.FileName);
+            }
+            
+        }
+        bool isStoped;
+        private void button_stop_Click(object sender, EventArgs e)
+        {
+            if (!isStoped)
+            {
+                isStoped = true;
+                button_stop.Text = "Продолжить";
+            }else
+            {
+                isStoped = false;
+                button_stop.Text = "Остановить";
+            }
+            if (!isStoped && pagesCount > 1 && currPage < pagesCount)
+            {
+                currPage++;
+                wb.Navigate(url + "&page=" + currPage);
+            }
+        }
+    }
+
+
+}
